b270d0948e9250ee1a6c82acb570257de605f357
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-08-24 22:21:38 +0300

    refactor: searchInfo

--- src/hooks/searchInfo.js
+++ src/hooks/searchInfo.js
@@ -7,7 +7,7 @@ function searchInfo(dataArray, key) {
 
   // поиск пьес
   const plays = dataArray.reduce((acc, current) => {
-    const check = current.title.toLowerCase().includes(searchKey) ? acc.push(current) : acc;
+    if (current.title.toLowerCase().includes(searchKey)) acc.push(current);
     return acc;
   }, [])
 
@@ -15,38 +15,43 @@ function searchInfo(dataArray, key) {
   const authors = dataArray.reduce((acc, current) => {
     // проверка авторов на дубли
     if (acc.find((item) => item.author_firstName === current.author_firstName || item.author_lastName === current.author_lastName)) return acc;
-    const check =
-      current.author_firstName.toLowerCase().includes(searchKey) ||
-        current.author_lastName.toLowerCase().includes(searchKey)
-        ? acc.push({
-          author_firstName: current.author_firstName,
-          author_lastName: current.author_lastName,
-          _id: current._id,
-        }) : acc;
+    if (current.author_firstName.toLowerCase().includes(searchKey) || current.author_lastName.toLowerCase().includes(searchKey)) {
+      acc.push({
+        author_firstName: current.author_firstName,
+        author_lastName: current.author_lastName,
+        _id: current._id,
+      })
+    }
     return acc;
   }, [])
 
-  // упаковка авторов в группы по первым буквам фамилии
-  const authorsGrouped = authors.reduce((acc, current) => {
+  return {
+    plays: sortElements(plays),
+    authors: sortElements(groupElementsByFirstLetter(authors))
+  }
+}
+
+// вспомогательные функции
+function groupElementsByFirstLetter(array) {
+  return array.reduce((acc, current) => {
     const group = current.author_lastName[0];
     if (!acc[group]) acc[group] = { group, children: [current], _id: current._id }
     else acc[group].children.push(current);
     return acc;
   }, {})
-
-  //сортировка групп авторов по алфавиту
-  const autorsArrSorted = Object.values(authorsGrouped).sort((a, b) => {
-    if (a.group < b.group)
-      return -1;
-    if (a.group > b.group)
-      return 1;
-    return 0;
-  })
-  return {
-    plays,
-    authors: autorsArrSorted
-  }
 }
 
+function sortElements(array) {
+  if (!Array.isArray(array)) {
+    return Object.values(array).sort(compareStrings)
+  };
+  return array.sort(compareStrings);
+}
+
+function compareStrings(a, b) {
+  if (a.group < b.group) return -1;
+  if (a.group > b.group) return 1;
+  return 0;
+}
 
 export default searchInfo;
